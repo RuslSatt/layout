901a0f41addf1880e652e4a80023f0994df94816
Ruslan Sattarov <r8sattarov@mail.ru>
2022-04-03 16:33:33 +0500

    feat: add header and a adaptive menu

--- allbirds/src/js/index.js
+++ allbirds/src/js/index.js
@@ -1,3 +1,35 @@
 import '../scss/project-allbirds.scss';
+import '../index.html'
 
+
+const iconMenu = document.querySelector('.icon-menu');
+const menu = document.querySelector('.menu');
+
+iconMenu.addEventListener('click', function (e) {
+    iconMenu.classList.toggle('active');
+    menu.classList.toggle('active');
+    document.body.classList.toggle('lock');
+})
+
+const linksLeft = document.querySelectorAll('[data-num = link]');
+// const linksRight = document.querySelectorAll('.menu-right__link');
+
+linksLeft.forEach(linkLeft => {
+    linkLeft.addEventListener('click', function (e) {
+        if (iconMenu.classList.contains('active')) {
+            iconMenu.classList.remove('active');
+            menu.classList.remove('active');
+            document.body.classList.remove('lock');
+        }
+    })
+})
+// linksRight.forEach(linkRight => {
+//     linkRight.addEventListener('click', function (e) {
+//         if (iconMenu.classList.contains('active')) {
+//             iconMenu.classList.remove('active');
+//             menu.classList.remove('active');
+//             document.body.classList.remove('lock');
+//         }
+//     })
+// })
 
--- allbirds/webpack.config.js
+++ allbirds/webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
 
 module.exports = {
    context: path.resolve(__dirname, 'src'),
@@ -13,14 +15,10 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'js/[name].[contenthash].js',
-      assetModuleFilename: './assets/images/[contenthash][ext][query]',
+      assetModuleFilename: 'assets/images/[contenthash][ext][query]',
       clean: true,
    },
-   resolve: {
-      extensions: ['.js', '.json',],
-   },
-   target: ['web', 'es5'],
-   devtool: 'inline-source-map',
+   target: 'web',
    devServer: {
       static: {
          directory: path.resolve(__dirname, 'dist'),
@@ -32,7 +30,7 @@ module.exports = {
    plugins: [
       new HtmlWebpackPlugin({
          filename: "index.html",
-         template: path.resolve(__dirname, 'src', 'index.html'),
+         template: path.resolve(__dirname, 'src/index.html'),
       }),
       new MiniCssExtractPlugin({
          filename: 'css/style.[contenthash].css',
@@ -46,7 +44,25 @@ module.exports = {
          },
          {
             test: /\.s?css$/,
-            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            use: [MiniCssExtractPlugin.loader,
+               'css-loader',
+               'sass-loader',
+               {
+                  loader: "postcss-loader",
+                  options: {
+                     postcssOptions: {
+                        plugins: [
+                           [
+                              "postcss-preset-env",
+                              {
+                                 // Options
+                              },
+                           ],
+                        ],
+                     },
+                  },
+               },
+            ]
          },
          {
             test: /\.(png|jpg|svg)$/,
@@ -82,6 +98,35 @@ module.exports = {
       minimizer: [
          new TerserWebpackPlugin(),
          new CssMinimizerWebpackPlugin(),
+         new ImageMinimizerPlugin ({
+            minimizer: {
+               implementation: ImageMinimizerPlugin.imageminMinify,
+               options: {
+                  plugins: [
+                     ["gifsicle", { interlaced: true }],
+                     ["jpegtran", { progressive: true }],
+                     ["optipng", { optimizationLevel: 5 }],
+                     [
+                        "svgo",
+                        {
+                           plugins: extendDefaultPlugins([
+                              {
+                                 name: "removeViewBox",
+                                 active: false,
+                              },
+                              {
+                                 name: "addAttributesToSVGElement",
+                                 params: {
+                                    attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                                 },
+                              },
+                           ]),
+                        },
+                     ],
+                  ],
+               },
+            },
+         }),
       ]
    }
 };
